dd908a0a5c50eb64b1dc46e5f1be147813625954
dykyys <dykyy.s@gmail.com>
2022-07-12 14:26:48 +0300

    completed

--- src/js/load-scripts.js
+++ src/js/load-scripts.js
@@ -5,6 +5,7 @@
 /*
  * Завантаження скриптів
  */
+
 //? Створіть функцію loadScript(url), яка буде створювати та додавати скрипт на сторінку
 //? Передайте кобэк, який буде викликатися по завершеню завантаженя скрипта
 
@@ -14,36 +15,31 @@
 
 //? Пекельна піраміда колбеків
 
-// const loadScript = (url, cb) => {
+// const loadScript = (url, callback) => {
 //   const script = document.createElement('script');
 //   script.src = url;
-
 //   document.body.append(script);
 
 //   script.addEventListener('load', () => {
-//     cb(script);
+//     callback(script);
 //   });
-
 //   script.addEventListener('error', () => {
-//     cb(null, 'Error');
+//     callback(null, 'Error');
 //   });
 // };
-
 // loadScript(
 //   'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js',
-//   (script, err) => {
+//   (script, error) => {
 //     if (script) {
 //       console.log(`${script.src} завантажився успішно`);
-
 //       loadScript(
 //         'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-//         (script, err) => {
+//         (script, error) => {
 //           if (script) {
 //             console.log(`${script.src} завантажився успішно`);
-
 //             loadScript(
 //               'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js',
-//               (script, err) => {
+//               (script, error) => {
 //                 if (script) {
 //                   console.log(`${script.src} завантажився успішно`);
 //                 } else {
@@ -63,41 +59,36 @@
 // );
 
 //? Рішення через проміси
-// const loadScript = url => {
-//   const script = document.createElement('script');
-//   script.src = url;
+const loadScript = url => {
+  const script = document.createElement('script');
+  script.src = url;
+  document.body.append(script);
 
-//   document.body.append(script);
-
-//   const promise = new Promise((resolve, reject) => {
-//     script.addEventListener('load', () => {
-//       resolve(script);
-//     });
-
-//     script.addEventListener('error', () => {
-//       reject('Error');
-//     });
-//   });
-
-//   return promise;
-// };
-
-// loadScript('https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js')
-//   .then(script => {
-//     console.log(`${script.src} завантажився успішно`);
-//     return loadScript(
-//       'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'
-//     );
-//   })
-//   .then(script => {
-//     console.log(`${script.src} завантажився успішно`);
-//     return loadScript(
-//       'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js'
-//     );
-//   })
-//   .then(script => {
-//     console.log(`${script.src} завантажився успішно`);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+  return new Promise((resolve, rejected) => {
+    script.addEventListener('load', () => {
+      resolve(script);
+    });
+    script.addEventListener('error', () => {
+      rejected('Error');
+    });
+  });
+};
+loadScript('https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js')
+  .then(script => {
+    console.log(`${script.src} завантажився успішно`);
+    return loadScript(
+      'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'
+    );
+  })
+  .then(script => {
+    console.log(`${script.src} завантажився успішно`);
+    return loadScript(
+      'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js'
+    );
+  })
+  .then(script => {
+    console.log(`${script.src} завантажився успішно`);
+  })
+  .catch(error => {
+    console.log(error);
+  });
